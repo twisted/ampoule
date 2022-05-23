c2f4331f105acfac5869910c487e7e9e92072088
Glyph <glyph@twistedmatrix.com>
2022-05-23 15:56:11 -0700

    update NEWS & version for 22.5.0 release

--- ampoule/_version.py
+++ ampoule/_version.py
@@ -7,5 +7,5 @@ Provides ampoule version information.
 
 from incremental import Version
 
-__version__ = Version('ampoule', 19, 12, 0)
+__version__ = Version('ampoule', 22, 5, 0)
 __all__ = ["__version__"]
